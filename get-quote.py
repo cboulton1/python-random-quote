072f8ded1fdb33263023901e595989181d4d939e
cboulton <95535468+cboulton1@users.noreply.github.com>
2021-12-05 15:33:02 +0000

    Print out a random number of quotes each time

--- get-quote.py
+++ get-quote.py
@@ -6,9 +6,13 @@ def primary():
   with open("quotes.txt") as f:
     quotes = f.readlines()
 
-  last = len(quotes) - 1 # last index in array
-  rnd = random.randint(0, last)
-  print(quotes[rnd].replace("\n", ""))
+  rnd_num_quotes = random.randint(1, len(quotes))
+  
+  for i in range(rnd_num_quotes):
+    last_index = len(quotes) - 1
+    rnd_index = random.randint(0, last_index)
+    print(quotes[rnd_index].replace("\n", ""))
+    quotes.remove(quotes[rnd_index])
 
 if __name__== "__main__":
   primary()
