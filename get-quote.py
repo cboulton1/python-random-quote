e940f5daa62877aaf8c4e443d716926610a6fe3a
cboulton <95535468+cboulton1@users.noreply.github.com>
2021-12-05 16:23:04 +0000

    Can add quotes programmatically

--- get-quote.py
+++ get-quote.py
@@ -1,13 +1,21 @@
 import random
 
 def primary():
-  #print("Keep it logically awesome.")
-
+  
+  # option to add quote(s) to quotes.txt
+  response = input("Add a quote? y/n ")
+  while response.lower() == "y":
+    quote = input("Type quote: ")
+    with open("quotes.txt", "a") as f:
+      f.writelines("\n" + quote)
+    print("Quote added")
+    response = input("Add another quote? y/n ")
+  
   with open("quotes.txt") as f:
     quotes = f.readlines()
 
+  # print a random number of quotes from quotes.txt
   rnd_num_quotes = random.randint(1, len(quotes))
-  
   for i in range(rnd_num_quotes):
     last_index = len(quotes) - 1
     rnd_index = random.randint(0, last_index)
